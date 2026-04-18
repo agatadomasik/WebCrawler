8b64d3d0b5baa510052cf211f0ef51e3d00fd581
agata <agata.domasik@gmail.com>
2026-04-19 00:56:40 +0200

    Implemented clustering coefficients analysis

--- WebCrawler.Graph/GraphStats.cs
+++ WebCrawler.Graph/GraphStats.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using ScottPlot;
+using System.Runtime.InteropServices;
 using WebCrawler.Domain;
 using WebCrawler.Graph;
 
@@ -34,6 +35,7 @@ namespace WebCrawler.Graph
             PowerLawFitter.FitMLE(outDegrees, "plots/outMLE.png");
 
             PrintBFSStats(graph);
+            PrintClusteringStats(graph);
         }
 
         public static void PrintBFSStats(CrawlGraph graph)
@@ -90,6 +92,96 @@ namespace WebCrawler.Graph
             Console.WriteLine($"\n{label} histogram (degree : count):");
             foreach (var g in groups)
                 Console.WriteLine($"  {g.Key,4} : {g.Count(),5}  {new string('█', Math.Min(g.Count() / 2, 40))}");
+        }
+
+        public static void PrintClusteringStats(CrawlGraph graph)
+        {
+            double[] localCoeffs = ClusteringAnalyzer.ComputeLocalClusteringCoefficients(graph);
+
+            int binCount = 10;
+            double binSize = 0.1;
+            double[] counts = new double[binCount];
+
+            foreach (var val in localCoeffs.Where(v => v >= 0))
+            {
+                int bin = (int)(val / binSize);
+                if (bin >= binCount) bin = binCount - 1;
+                counts[bin]++;
+            }
+
+            string[] labels = Enumerable.Range(0, binCount)
+                .Select(i => $"{i * binSize:F1}–{(i + 1) * binSize:F1}")
+                .ToArray();
+
+            var plt = new Plot();
+
+            double[] positions = Enumerable.Range(0, binCount).Select(i => (double)i).ToArray();
+            var bar = plt.Add.Bars(positions, counts);
+
+            plt.Axes.Bottom.SetTicks(positions, labels);
+            plt.Axes.Bottom.Label.Text = "Interval";
+            plt.Axes.Left.Label.Text = "Number of values";
+            plt.Title("Local clustering coefficients");
+
+            plt.SavePng("plots/LocalClusteringCoefficients.png", 800, 500);
+
+            var avgCByDegree = graph.OutDegrees
+                .Zip(localCoeffs, (deg, c) => (deg, c))
+                .Where(x => x.c != -1)
+                .GroupBy(x => x.deg)
+                .ToDictionary(
+                    g => g.Key,
+                    g => g.Average(x => x.c)
+                );
+            var points = avgCByDegree
+                .Where(x => x.Key > 0 && x.Value > 0)
+                .OrderBy(x => x.Key)
+                .ToArray();
+
+            double[] ks = points.Select(x => (double)x.Key).ToArray();
+            double[] avgCs = points.Select(x => x.Value).ToArray();
+
+            double[] logK = ks.Select(Math.Log10).ToArray();
+            double[] logC = avgCs.Select(Math.Log10).ToArray();
+
+            int n = logK.Length;
+            double sumX = logK.Sum();
+            double sumY = logC.Sum();
+            double sumXY = logK.Zip(logC, (x, y) => x * y).Sum();
+            double sumX2 = logK.Select(x => x * x).Sum();
+
+            double slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
+            double intercept = (sumY - slope * sumX) / n;
+
+            Console.WriteLine($"Nachylenie (exponent): {slope:F4}  (oczekiwane: -1)");
+            Console.WriteLine($"Współczynnik:          {Math.Pow(10, intercept):F4}");
+
+            // Linia regresji
+            double[] fitX = new[] { logK.Min(), logK.Max() };
+            double[] fitY = fitX.Select(x => slope * x + intercept).ToArray();
+
+            // Wykres
+            plt = new Plot();
+
+            // Punkty danych
+            var scatter = plt.Add.Scatter(logK, logC);
+            scatter.LineWidth = 0;
+            scatter.MarkerSize = 6;
+            scatter.LegendText = "C(k) empiryczne";
+
+            // Linia dopasowania
+            var line = plt.Add.Scatter(fitX, fitY);
+            line.MarkerSize = 0;
+            line.LineWidth = 2;
+            line.LinePattern = LinePattern.Dashed;
+            line.LegendText = $"fit: k^{slope:F2}";
+
+            plt.Axes.Bottom.Label.Text = "log(k)";
+            plt.Axes.Left.Label.Text = "log(C(k))";
+            plt.Title("C(k) vs k  [log-log]");
+            plt.ShowLegend();
+
+            plt.SavePng("plots/ck_loglog.png", 800, 500);
         }
     }
 }
